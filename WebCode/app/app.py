c03bbf64d3cca0e56a947c71e8ac2ad8d2a28425
Simon Vernon <70655906+cy63rSANS@users.noreply.github.com>
2024-09-17 12:29:55 +0100

    webcode and cloud init updates

--- WebCode/app/app.py
+++ WebCode/app/app.py
@@ -2,26 +2,34 @@ from flask import Flask, render_template, request
 import os
 import logging
 import logging.handlers
+from datetime import datetime
 
 app = Flask(__name__)
 
+# Function to get the current timestamp
+def get_timestamp():
+    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
 # Function to log an error to Apache
 def log_to_apache(error_message):
+    timestamp = get_timestamp()
     with open("/var/log/apache2/error.log", "a") as apache_log:
-        apache_log.write(f"{error_message}\n")
+        apache_log.write(f"[{timestamp}] {error_message}\n")
 
 # Function to log an error to Syslog
 def log_to_syslog(error_message):
+    timestamp = get_timestamp()
     syslog = logging.getLogger('SyslogLogger')
     syslog.setLevel(logging.INFO)
     handler = logging.handlers.SysLogHandler(address='/dev/log')
     syslog.addHandler(handler)
-    syslog.info(error_message)
+    syslog.info(f"[{timestamp}] {error_message}")
 
 # Function to log an error to Secure log
 def log_to_secure(error_message):
+    timestamp = get_timestamp()
     with open("/var/log/auth.log", "a") as secure_log:
-        secure_log.write(f"{error_message}\n")
+        secure_log.write(f"[{timestamp}] {error_message}\n")
 
 # Flask Route for logging error
 @app.route('/log_error', methods=['POST'])
